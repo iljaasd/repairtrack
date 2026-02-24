e595c1ba6a003fa1da4d2efb7beb0c8a26aae74c
iljaasd <iljaasd@gmail.com>
2026-02-24 23:57:22 +0100

    init commit

---
+++ src/main/java/com/mod/Repairtrack/RepairtrackApplication.java
@@ -0,0 +1,13 @@
+package com.mod.Repairtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairtrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mod/Repairtrack/RepairtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mod.Repairtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepairtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
