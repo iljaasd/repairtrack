f9a152c7ced89a8ed127c86ee9f83ef3be367d5b
iljaasd <iljaasd@gmail.com>
2026-02-25 06:14:09 +0100

    - empty controller created for vehicle

--- src/main/java/com/mod/Repairtrack/modules/vehicle/controller/v1/VehicleController.java
+++ src/main/java/com/mod/Repairtrack/modules/vehicle/controller/v1/VehicleController.java
@@ -1,7 +1,9 @@
 package com.mod.Repairtrack.modules.vehicle.controller.v1;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value = "api/v1/")
 public class VehicleController {
 }
